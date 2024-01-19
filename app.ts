a6569ff549e4d45cfa28eac89ac97322f998cc85
joy <joy.bhuiya@bjitgroup.com>
2024-01-19 17:52:18 +0600

    Get All Books is Implemented

--- app.ts
+++ app.ts
@@ -1,12 +1,33 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./config/database";
+import cors from "cors";
+import { CustomError } from "./interfaces/error/customError_interface";
+import bookRoute from "./routes/book/bookRoute";
 
 const app = express();
-const port = 5000;
+const PORT = 8000;
+dotenv.config();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+app.use("/book", bookRoute);
+
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).send({ message: "Invalid JSON Format" });
+  } else {
+    next(err);
+  }
+});
+app.use((req: Request, res: Response) => {
+  return res.status(400).send({ message: "Bad Request" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+databaseConnection(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is Running on ${PORT} port`);
+  });
 });
